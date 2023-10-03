c539680ede3b1668739ea4b35c48062c9b8e76e8
vaibhav1290 <vaibhavsmahar.tech@gmail.com>
2023-10-03 19:39:43 +0530

    auth built completely

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Auth/Login/Login";
+import Register from "./components/Auth/Signup/Register";
+import Reset from "./components/Auth/Reset/Reset";
+import Dashboard from "./components/Dashboard/Dashboard";
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
