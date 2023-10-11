a13c872a7d378442b2f328cd2d19ea67bc950e1f
vaibhav1290 <vaibhavsmahar.tech@gmail.com>
2023-10-11 11:01:10 +0530

    account popover built

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,10 +3,29 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth, db, logout } from "../../firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
-import Button from '@mui/material/Button';
+
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
+import Popover from '@mui/material/Popover';
+import { alpha } from '@mui/material/styles';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+
+const MENU_OPTIONS = [
+  {
+    label: 'Profile',
+    icon: 'eva:person-fill',
+  },
+  {
+    label: 'Settings',
+    icon: 'eva:settings-2-fill',
+  },
+]
 
 function Dashboard() {
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const [name, setName] = useState("");
   const navigate = useNavigate();
 
@@ -30,16 +49,79 @@ function Dashboard() {
     fetchUserName();
   }, [user, loading]);
 
+  const [open, setOpen] = useState(null);
+
+  const handleOpen = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
   return (
-    <div className="dashboard">
-      <div className="dashboard__container">
-        <div>{name}</div>
-        <div>{user?.email}</div>
-        <Button variant="contained" onClick={logout}>
+    <>
+    <IconButton
+        onClick={handleOpen}
+        sx={{
+          width: 40,
+          height: 40,
+          background: (theme) => alpha(theme.palette.grey[500], 0.08),
+          ...(open && {
+            background: (theme) =>
+              `linear-gradient(135deg, ${theme.palette.primary.light} 0%, ${theme.palette.primary.main} 100%)`,
+          }),
+        }}
+      >
+        <Avatar
+          src={user?.photoURL}
+          alt={name}
+          sx={{
+            width: 36,
+            height: 36,
+            border: (theme) => `solid 2px ${theme.palette.background.default}`,
+          }}
+        >
+          {name.charAt(0).toUpperCase()}
+        </Avatar>
+      </IconButton>
+
+      <Popover
+        open={!!open}
+        anchorEl={open}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <Box sx={{ my: 1.5, px: 2 }}>
+          <Typography variant="subtitle2" noWrap>
+            {name}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+            {user?.email}
+          </Typography>
+        </Box>
+
+        <Divider sx={{ borderStyle: 'dashed' }} />
+
+        {MENU_OPTIONS.map((option) => (
+          <MenuItem key={option.label} onClick={handleClose}>
+            {option.label}
+          </MenuItem>
+        ))}
+
+        <Divider sx={{ borderStyle: 'dashed', m: 0 }} />
+
+        <MenuItem
+          disableRipple
+          disableTouchRipple
+          onClick={logout}
+          sx={{ typography: 'body2', color: 'error.main', py: 1.5 }}
+        >
           Logout
-        </Button>
-      </div>
-    </div>
+        </MenuItem>
+      </Popover>
+    </>
   );
 }
 
