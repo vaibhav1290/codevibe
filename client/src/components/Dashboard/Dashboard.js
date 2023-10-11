1dc4e9ef57dbcd9411b8579dd9324d6863fd75c5
vaibhav1290 <vaibhavsmahar.tech@gmail.com>
2023-10-11 17:28:33 +0530

    dashboard initialized

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -12,6 +12,20 @@ import { alpha } from '@mui/material/styles';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import { styled, useTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const drawerWidth = 200;
 
 const MENU_OPTIONS = [
   {
@@ -59,67 +73,173 @@ function Dashboard() {
     setOpen(null);
   };
 
+  const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: `-${drawerWidth}px`,
+      ...(open && {
+        transition: theme.transitions.create('margin', {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+      }),
+    }),
+  );
+  
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
+  
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
+
+  const theme = useTheme();
+  const [open_d, setOpen_d] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen_d(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen_d(false);
+  };
+
   return (
     <>
-    <IconButton
-        onClick={handleOpen}
-        sx={{
-          width: 40,
-          height: 40,
-          background: (theme) => alpha(theme.palette.grey[500], 0.08),
-          ...(open && {
-            background: (theme) =>
-              `linear-gradient(135deg, ${theme.palette.primary.light} 0%, ${theme.palette.primary.main} 100%)`,
-          }),
-        }}
-      >
-        <Avatar
-          src={user?.photoURL}
-          alt={name.charAt(0).toUpperCase()}
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open_d} sx={{backgroundColor: 'transparent', boxShadow: 'none'}}>
+        <Toolbar sx={{ display: 'flex'}}>
+          <IconButton
+          color='#000'
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{ mr: 2, ...(open_d && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1 }}></Typography>
+          
+          <IconButton
+          onClick={handleOpen}
           sx={{
-            width: 36,
-            height: 36,
-            border: (theme) => `solid 2px ${theme.palette.background.default}`,
+            width: 40,
+            height: 40,
+            background: (theme) => alpha(theme.palette.grey[500], 0.08),
+            ...(open && {
+              background: (theme) =>
+                `linear-gradient(135deg, ${theme.palette.primary.light} 0%, ${theme.palette.primary.main} 100%)`,
+            }),
           }}
-        />
+          >
+            <Avatar
+            src={user?.photoURL}
+            alt={name.charAt(0).toUpperCase()}
+            sx={{
+              width: 36,
+              height: 36,
+              border: (theme) => `solid 2px ${theme.palette.background.default}`,
+            }}/>
+          </IconButton>
+        
+          <Popover
+          open={!!open}
+          anchorEl={<Avatar/>}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          >
+            <Box sx={{ my: 1.5, px: 2 }}>
+              <Typography variant="subtitle2" noWrap>
+                {name}
+             </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+                {user?.email}
+              </Typography>
+           </Box>
+
+            <Divider sx={{ borderStyle: 'dashed' }} />
+
+              {MENU_OPTIONS.map((option) => (
+              <MenuItem MenuItem key={option.label} onClick={handleClose}>
+                {option.label}
+              </MenuItem>
+            ))}
+          
+            <Divider sx={{ borderStyle: 'dashed', m: 0 }} />
+          
+            <MenuItem
+            disableRipple
+            disableTouchRipple
+            onClick={logout}
+            sx={{ typography: 'body2', color: 'error.main', py: 1.5 }}
+           >
+              Logout
+            </MenuItem>
+          </Popover>
+        </Toolbar>
+      </AppBar>
+    <Drawer
+    sx={{
+    width: drawerWidth,
+    flexShrink: 0,
+    '& .MuiDrawer-paper': {
+    width: drawerWidth,
+    boxSizing: 'border-box',
+    },}}
+    variant="persistent"
+    anchor="left"
+    open={open_d}
+    >
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
       </IconButton>
-
-      <Popover
-        open={!!open}
-        anchorEl={open}
-        onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      >
-        <Box sx={{ my: 1.5, px: 2 }}>
-          <Typography variant="subtitle2" noWrap>
-            {name}
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
-            {user?.email}
-          </Typography>
-        </Box>
-
-        <Divider sx={{ borderStyle: 'dashed' }} />
-
-        {MENU_OPTIONS.map((option) => (
-          <MenuItem key={option.label} onClick={handleClose}>
-            {option.label}
-          </MenuItem>
-        ))}
-
-        <Divider sx={{ borderStyle: 'dashed', m: 0 }} />
-
-        <MenuItem
-          disableRipple
-          disableTouchRipple
-          onClick={logout}
-          sx={{ typography: 'body2', color: 'error.main', py: 1.5 }}
-        >
-          Logout
-        </MenuItem>
-      </Popover>
-    </>
+    </DrawerHeader>
+    <List>
+      {['Home', 'Chat', 'Meeting', 'Code Editor'].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton>
+            <ListItemText primary={text} />
+          </ListItemButton>
+        </ListItem>))}
+      </List>    
+    </Drawer>
+    <Main open={open_d}>
+      {/* Content Home */}
+    </Main>
+  </Box>
+</>
   );
 }
 
