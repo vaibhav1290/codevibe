1aad0bc79237053bc540748253d72bb9077cbf13
vaibhav1290 <vaibhavsmahar.tech@gmail.com>
2023-12-24 10:37:32 +0530

    added vercel insights and analytics

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </>
+);
